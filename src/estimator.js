5c851ddf43cd98dc8a72ef0e635b12031a92faca
ayor <39896394+ayor@users.noreply.github.com>
2020-04-12 11:19:18 +0100

    another change

--- src/estimator.js
+++ src/estimator.js
@@ -1,40 +1,33 @@
 const data = {
-    region: {
-    name: "Africa",
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-    },
-    periodType: "days",
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614
-    }
+  region: {
+  name: 'Africa',
+  avgAge: 19.7,
+  avgDailyIncomeInUSD: 5,
+  avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
 
-    const currentlyInfected = (data.reportedCases * 10).toFixed(0);
-    const infectionsByRequestedTime = currentlyInfected * 512
-    const severeImpactCurrentlyInfectedCases = (data.reportedCases * 50).toFixed(0)
+const currentlyInfected = (data.reportedCases * 10).toFixed(0);
+const infectionsByRequestedTime = currentlyInfected * 512;
+const severeImpactCurrentlyInfectedCases = (data.reportedCases * 50).toFixed(0);
 
-    const impact = {
-        currentlyInfected,
-        infectionsByRequestedTime
+const impact = {
+  currentlyInfected,
+  infectionsByRequestedTime
+};
 
-    }
-
-    const severeImpact={
-        currentlyInfected: severeImpactCurrentlyInfectedCases,
-        infectionsByRequestedTime: severeImpactCurrentlyInfectedCases * 512
-    }
+const severeImpact = {
+  currentlyInfected: severeImpactCurrentlyInfectedCases,
+  infectionsByRequestedTime: severeImpactCurrentlyInfectedCases * 512
+};
 
 const covid19ImpactEstimator = (data) => {
-    const input = data
-    return(
-        {
-            data: input,
-            impact,
-            severeImpact
-        }
-    )};
+  return ({data,impact,severeImpact});
+};
 
 export default covid19ImpactEstimator;
