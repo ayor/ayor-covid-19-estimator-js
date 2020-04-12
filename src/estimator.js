ceecf134c2aa19f3f377f124ec256eb3fa3bdcf7
ayor <39896394+ayor@users.noreply.github.com>
2020-04-12 11:26:17 +0100

    final

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,16 @@
-const data = {
-  region: {
-  name: 'Africa',
-  avgAge: 19.7,
-  avgDailyIncomeInUSD: 5,
-  avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
+// const data = {
+//     region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
 
 const currentlyInfected = (data.reportedCases * 10).toFixed(0);
 const infectionsByRequestedTime = currentlyInfected * 512;
@@ -26,8 +26,6 @@ const severeImpact = {
   infectionsByRequestedTime: severeImpactCurrentlyInfectedCases * 512
 };
 
-const covid19ImpactEstimator = (data) => {
-  return ({data,impact,severeImpact});
-};
+const covid19ImpactEstimator = (data) => ({ data, impact, severeImpact });
 
 export default covid19ImpactEstimator;
