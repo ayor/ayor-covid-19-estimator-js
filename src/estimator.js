9a1c9bdff8150267817f386865f6b4a70400443e
ayor <39896394+ayor@users.noreply.github.com>
2020-04-12 11:30:31 +0100

    another final change

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,3 @@
-// const data = {
-//     region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
-
 const currentlyInfected = (data.reportedCases * 10).toFixed(0);
 const infectionsByRequestedTime = currentlyInfected * 512;
 const severeImpactCurrentlyInfectedCases = (data.reportedCases * 50).toFixed(0);
@@ -26,6 +12,8 @@ const severeImpact = {
   infectionsByRequestedTime: severeImpactCurrentlyInfectedCases * 512
 };
 
-const covid19ImpactEstimator = (data) => ({ data, impact, severeImpact });
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return({ data: input, impact, severeImpact })};
 
 export default covid19ImpactEstimator;
