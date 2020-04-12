4ecb460ef8dabe204c6f9d4e6feb90a529fe3a8c
ayor <39896394+ayor@users.noreply.github.com>
2020-04-12 09:37:48 +0100

    reset estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,47 +1,4 @@
-const covid19ImpactEstimator = (data) =>{
-// //to estiamte the number of infected people 28 days from now
-//     //infections after 28 days  currentlyInfected * 512       
-//     //sending out data impact
-//     //impact = { currentlyInfected : data.reportedCases * 10}
-
-//     const impactCurrentinfectedValue = data.reportedCases * 10;
-//     const severeCurrentinfectedValue = data.reportedCases * 50;
-
-//     //infections by requested time
-//     const impactinfectionsByRequestedTime = impactCurrentinfectedValue * 512;
-//     const severeinfectionsByRequestedTime = severeCurrentinfectedValue * 512;
-
-//         //severe cases by requested time
-//         const impactSevereCasesByRequestedTime = 0.15 * impactinfectionsByRequestedTime
-//         const severeimpactSevereCasesByRequestedTime = 0.15 * severeinfectionsByRequestedTime
-
-//     //EXPECTED hosptial beds 
-//     const expectedBeds = data.totalHospitalBeds * 0.35
-
-//     //dollars in flight calculation 
-//     const impactDollarsinFight = (impactinfectionsByRequestedTime * 0.65 * 1.5) / 30;
-
-
-//     let impactEntry = {
-//         currentlyInfected :  impactCurrentinfectedValue.toFixed(0),
-//         //infections in 28 days
-//         infectionsByRequestedTime :impactinfectionsByRequestedTime.toFixed(0),
-//         severeCasesByRequestedTime : impactSevereCasesByRequestedTime.toFixed(0),
-//         hospitalBedsByRequestedTime: expectedBeds - impactSevereCasesByRequestedTime.toFixed(0),
-//         casesForICUByRequestedTime: 0.05 * impactinfectionsByRequestedTime.toFixed(0),
-//         casesForVentilatorsByRequestedTime : 0.02 * impactinfectionsByRequestedTime.toFixed(0),
-//         dollarsInFlight : impactDollarsinFight
-//     };
-//     let severeImpactEntry = {
-//         currentlyInfected :  severeCurrentinfectedValue.toFixed(0),
-//         infectionsByRequestedTime : severeinfectionsByRequestedTime.toFixed(0), //infections in 28 days
-//         severeCasesByRequestedTime : severeimpactSevereCasesByRequestedTime.toFixed(0),
-//         hospitalBedsByRequestedTime : expectedBeds - severeimpactSevereCasesByRequestedTime.toFixed(0),
-//         casesForICUByRequestedTime: 0.05 * severeinfectionsByRequestedTime.toFixed(0),
-//         casesForVentilatorsByRequestedTime: 0.02 * severeinfectionsByRequestedTime.toFixed(0),
-//         dollarsInFlight : severeImpactDollarsinFight.toFixed(0)
-//     };
-    
+const covid19ImpactEstimator = (data) => {
     return(
         {
             data,
