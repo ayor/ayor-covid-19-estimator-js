7ca64dc23a858e75712bffe400137396d8660b35
ayor <39896394+ayor@users.noreply.github.com>
2020-04-13 07:49:20 +0100

    restructured code 4

--- src/estimator.js
+++ src/estimator.js
@@ -3,16 +3,18 @@ const covid19ImpactEstimator = (data) => {
 
   if (data.periodType === 'days') {
     factor = (data.timeToElapse / 3);
-  } else if (data.periodType === 'weeks') {
+  } 
+  if (data.periodType === 'weeks') {
     factor = ((data.timeToElapse * 7) / 3);
-  } else {
+  } 
+  if (data.periodType === 'months') {
     factor = ((data.timeToElapse * 30) / 3);
   }
 
   const currentlyInfected = Math.trunc(data.reportedCases * 10);
   const infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** Math.trunc(factor)));
   const severeImpactCurrInfectedCases = Math.trunc(data.reportedCases * 50);
-  const severImpactinfecTime = Math.trunc(severeImpactCurrInfectedCases * (2 ** Math.trunc(factor)));
+  const sevImpactinfecTime = Math.trunc(severeImpactCurrInfectedCases * (2 ** Math.trunc(factor)));
 
   const impact = {
     currentlyInfected,
@@ -21,7 +23,7 @@ const covid19ImpactEstimator = (data) => {
 
   const severeImpact = {
     currentlyInfected: severeImpactCurrInfectedCases,
-    infectionsByRequestedTime: severImpactinfecTime
+    infectionsByRequestedTime: sevImpactinfecTime
   };
 
   return (
