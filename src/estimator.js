922caa9266af46dbefca60e9240231ff4fa611c3
ayor <39896394+ayor@users.noreply.github.com>
2020-04-13 14:01:44 +0100

    003-04

--- src/estimator.js
+++ src/estimator.js
@@ -18,11 +18,18 @@ const covid19ImpactEstimator = (data) => {
   const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
   const sevImpactinfecTime = (severeImpactCurrInfectedCases * Math.trunc(2 ** Math.trunc(factor)));
   const severeimpactSevereCasesByRequestedTime = Math.trunc(0.15 * sevImpactinfecTime);
-  const dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5) / 30);
+  let dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5));
 
   const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
   const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
   const hospitalBedsByRequestedTime = Math.trunc(expectedBeds - severeCasesByRequestedTime);
+
+  if (data.periodType === 'weeks') {
+    dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5) / data.timeToElapse);
+  }
+  if (data.periodType === 'months') {
+    dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5) / data.timeToElapse);
+  }
 
   const impact = {
     currentlyInfected,
