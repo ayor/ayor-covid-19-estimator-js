7afcf657ef31b404620472ef4cb57ab9edd0a596
ayor <39896394+ayor@users.noreply.github.com>
2020-04-13 08:47:54 +0100

    challenge 2 major

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,24 @@
 const covid19ImpactEstimator = (data) => {
   let factor = 0;
+  let expectedBeds = Math.trunc(data.totalHospitalBeds * 0.35);
+  let casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
+  let casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
+  
 
   if (data.periodType === 'days') {
     factor = (data.timeToElapse / 3);
   }
   if (data.periodType === 'weeks') {
     factor = ((data.timeToElapse * 7) / 3);
+    expectedBeds = Math.trunc(data.totalHospitalBeds * 7 * 0.35);
+    casesForICUByRequestedTime = Math.trunc(0.05 * 7 * infectionsByRequestedTime);
+    casesForVentilatorsByRequestedTime = Math.trunc(0.02 * 7 * infectionsByRequestedTime);
   }
   if (data.periodType === 'months') {
     factor = ((data.timeToElapse * 30) / 3);
+    expectedBeds = Math.trunc(data.totalHospitalBeds * 30 * 0.35);
+    casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime * 30);
+    casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime * 30); 
   }
 
   const currentlyInfected = Math.trunc(data.reportedCases * 10);
@@ -16,11 +26,8 @@ const covid19ImpactEstimator = (data) => {
   const severeImpactCurrInfectedCases = Math.trunc(data.reportedCases * 50);
   const sevImpactinfecTime = Math.trunc(severeImpactCurrInfectedCases * (2 ** Math.trunc(factor)));
   const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
-  const severeimpactSevereCasesByRequestedTime = Math.trunc(0.15 * sevImpactinfecTime);
-  const expectedBeds = Math.trunc(data.totalHospitalBeds * 0.35);
+  const severeimpactSevereCasesByRequestedTime = Math.trunc(0.15 * sevImpactinfecTime);  
   const dollarsinFight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5) / 30);
-  const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
-  const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
   const hospitalBedsByRequestedTime = expectedBeds - severeCasesByRequestedTime;
 
   const impact = {
