08db130ba574abb7a692b0b8b002577a4ec11e5d
ayor <39896394+ayor@users.noreply.github.com>
2020-04-13 07:32:01 +0100

    restructured code

--- src/estimator.js
+++ src/estimator.js
@@ -1,38 +1,37 @@
 const covid19ImpactEstimator = (data) => {
+    let factor = 0;
 
-let factor = 0;
-
-if (data.periodType === 'days') {
-  factor = (data.timeToElapse / 3);
-} else if (data.periodType === 'weeks') {
-  factor = ((data.timeToElapse * 7) / 3);
-} else {
-  factor = ((data.timeToElapse * 30) / 3);
-}
-
-const currentlyInfected = Math.trunc(data.reportedCases * 10);
-const infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** Math.trunc(factor)));
-const severeImpactCurrInfectedCases = Math.trunc(data.reportedCases * 50);
-const severImpactinfecTime = Math.trunc(severeImpactCurrInfectedCases * (2 ** Math.trunc(factor)));
-
-console.log(currentlyInfected,infectionsByRequestedTime,severeImpactCurrInfectedCases,severImpactinfecTime);
-const impact = {
-  currentlyInfected,
-  infectionsByRequestedTime
-};
-
-const severeImpact = {
-  currentlyInfected: severeImpactCurrInfectedCases,
-  infectionsByRequestedTime: severImpactinfecTime
-};
-
-  return (
-    {
-     data,
-     impact,
-    severeImpact 
+    if (data.periodType === 'days') {
+        factor = (data.timeToElapse / 3);
+    } else if (data.periodType === 'weeks') {
+        factor = ((data.timeToElapse * 7) / 3);
+    } else {
+        factor = ((data.timeToElapse * 30) / 3);
     }
-  )
+
+    const currentlyInfected = Math.trunc(data.reportedCases * 10);
+    const infectionsByRequestedTime = Math.trunc(currentlyInfected * (2 ** Math.trunc(factor)));
+    const severeImpactCurrInfectedCases = Math.trunc(data.reportedCases * 50);
+    const severImpactinfecTime = Math.trunc(severeImpactCurrInfectedCases * (2 ** Math.trunc(factor)));
+
+    console.log(currentlyInfected, infectionsByRequestedTime, severeImpactCurrInfectedCases, severImpactinfecTime);
+    const impact = {
+        currentlyInfected,
+        infectionsByRequestedTime
+    };
+
+    const severeImpact = {
+        currentlyInfected: severeImpactCurrInfectedCases,
+        infectionsByRequestedTime: severImpactinfecTime
+    };
+
+    return (
+        {
+            data,
+            impact,
+            severeImpact
+        }
+    )
 };
 
 
