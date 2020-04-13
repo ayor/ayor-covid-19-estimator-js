f261d372bf96467a5846505f8235f714007b9fc2
ayor <39896394+ayor@users.noreply.github.com>
2020-04-13 14:14:34 +0100

    003-05

--- src/estimator.js
+++ src/estimator.js
@@ -18,7 +18,7 @@ const covid19ImpactEstimator = (data) => {
   const severeCasesByRequestedTime = Math.trunc(0.15 * infectionsByRequestedTime);
   const sevImpactinfecTime = (severeImpactCurrInfectedCases * Math.trunc(2 ** Math.trunc(factor)));
   const severeimpactSevereCasesByRequestedTime = Math.trunc(0.15 * sevImpactinfecTime);
-  let dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5)/data.timeToElapse);
+  const dollarsInFlight = Math.trunc((infectionsByRequestedTime * 0.65 * 1.5) / data.timeToElapse);
 
   const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
   const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
